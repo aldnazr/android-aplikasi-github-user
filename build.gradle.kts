a564d2724b52476c7435e14e66fb7df74d8e4e4c
anythinks <alauddinazhary05@gmail.com>
2025-08-14 16:58:37 +0700

    Upgrade Gradle version and plugins
    This commit upgrades the Gradle version from 8.6 to 8.7.
    It also updates the Android Gradle Plugin (AGP) from 8.3.0 to 8.6.0,
    the Kotlin Android plugin from 1.9.23 to 2.2.10,
    and the KSP plugin from 1.9.23-1.0.19 to 2.2.10-RC2-2.0.2.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 buildscript {
-    val agp_version by extra("8.3.0")
+    val agp_version by extra("8.6.0")
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
+    id("com.android.application") version "8.6.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
+    id("com.google.devtools.ksp") version "2.2.10-RC2-2.0.2" apply false
 }
